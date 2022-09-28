b5f6a59784bb5567ae2dd800aab3b5a0f0342929
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-29 00:00:14 +0600

    Cart component completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,7 +12,7 @@ const Cart = () => {
                     <p>Sylhet, Bangladesh</p>
                 </div>
             </div>
-            <div className='d-flex justify-content-between mt-3 bg-info px-4 py-1 rounded-lg'>
+            <div className='d-flex justify-content-between mt-3 bg-light px-4 py-1 rounded-lg'>
                 <div>
                     <h3 className='m-0'>55kg</h3>
                     <p>Weight</p>
@@ -25,6 +25,27 @@ const Cart = () => {
                     <h3 className='m-0'>23</h3>
                     <p>Age</p>
                 </div>
+            </div>
+            <div className='m-4'>
+                <h3>Add A Break</h3>
+                <div className='break-time bg-light p-3'>
+                    <button><small>10</small>M</button>
+                    <button><small>20</small>M</button>
+                    <button><small>30</small>M</button>
+                    <button><small>40</small>M</button>
+                    <button><small>50</small>M</button>
+                </div>
+            </div>
+            <div className='time-details'>
+                <h4>Exercise Details</h4>
+                <div className='d-flex justify-content-between p-2 bg-light'>
+                    <h6>Exercise Time</h6>
+                    <p><small>0</small> Minutes</p>
+                </div>
+            </div>
+            <div className='d-flex justify-content-between mt-3 bg-light p-2'>
+                <h4>Break Time</h4>
+                <p><small>0</small> Minutes</p>
             </div>
         </div>
     );
